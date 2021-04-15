5522febf208ca4d5fdbd70f4aecb3cf612d788b6
Musafak <musafak.ft18@mail.umy.ac.id>
2021-04-15 08:58:18 +0700

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Tambah(double a, double b)
+        {
+            return a + b;
+        }
+        public double Kali(double a, double b)
+        {
+            return a * b;
+        }
+        public double Bagi(double a, double b)
+        {
+            return a / b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Double.Parse(args[0]);
+            double b = Double.Parse(args[1]);
+            Calculator cc = new Calculator();
+            double tb = cc.Tambah(a, b);
+            double kl = cc.Kali(a, b);
+            double bg = cc.Bagi(a, b);
+            Console.WriteLine("Penjumlahan = {0}", tb);
+            Console.WriteLine("Perkalian = {0}", kl);
+            Console.WriteLine("Pembagian = {0}", bg);
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestFixture()]
+    public class CalculatorTests
+    {
+        //membuat obyek baru Calculator
+        Calculator cc = new Calculator();
+
+        [SetUp]
+        public void init()
+        {
+            cc = new Calculator();
+        }
+
+        [Test()]
+        public void TambahTest()
+        {
+            //Assert.Fail();
+            double hasilTb = cc.Tambah(4, 5);
+            Assert.AreEqual(9, hasilTb);
+        }
+
+        [Test()]
+        public void KaliTest()
+        {
+            //Assert.Fail();
+            double hasilKl = cc.Kali(4, 5);
+            Assert.AreEqual(20, hasilKl);
+        }
+
+        [Test()]
+        public void BagiTest()
+        {
+            //Assert.Fail();
+            double hasilBg = cc.Bagi(4, 5);
+            Assert.AreEqual(0.8, hasilBg);
+        }
+    }
+}
